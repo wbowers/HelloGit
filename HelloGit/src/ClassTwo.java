f2d08d94c2ecb748c3f8e483006c764d6144e0f9
wbowers <walt.bowers.jr@gmail.com>
2015-06-22 13:00:35 -0400

    another

---
+++ HelloGit/src/ClassTwo.java
@@ -0,0 +1,13 @@
+
+public class ClassTwo {
+
+	public ClassTwo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
